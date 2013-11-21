59c52f2ae48a6d868a7e67dbc8e99218aedcd00c
shijith <shibin@ubuntu.(none)>
2013-11-21 23:55:12 +0530

    Commiting source code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wishcart::Application.config.secret_key_base = '35f5cb9e4fa58fbf95094f1eba21effa635a0547e0b0d4ffc3e5dfc4cc8e58b29010cada87cf68990f27d352c4d50f9e19ace26fa6f9d845844c0cc6e838696c'
