d5b5c15075d1dc74c6334b383ab8702db9efb694
shibin <shibin@qburst.com>
2013-11-22 13:26:22 +0530

    added cart

--- db/migrate/20131120103123_create_products.rb
+++ db/migrate/20131120103123_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration
     create_table :products do |t|
       t.string :title
       t.text :description
-      t.string :image_url, :length => 20
+      t.string :image_url
       t.decimal :price, :precision => 8, :scale => 3
 
       t.timestamps
